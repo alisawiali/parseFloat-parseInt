8d0adefb737fb55636c03f7efb21e770f7eb0dc0
alisawiali <alisawiali70@gmail.com>
2023-04-19 10:35:14 +0200

    Swaping

---
+++ destruct-Array-Swap.js
@@ -0,0 +1,17 @@
+
+
+/*
+  -------> Destructuring  تدمير
+  -------> Swap تبديل
+   . . . .  Destructuring Array  => Swapping VAriables
+*/
+
+
+let bock = "video";
+let video = "Bock";
+
+[bock,video] = [video,bock]
+
+
+console.log(bock, ' => bock')
+console.log(video,'=> video')
--- destructuring-Array-Tow.js
+++ destructuring-Array-Tow.js
@@ -2,36 +2,23 @@
  -- Desruturcturing 
   ---- Desruturcturing Array Advanced Examples 
 */
-console.clear();
-let myFriends = [
-  "Imad",
-  "Ali",
-  "Jasem",
-  "Abd",
-  ["Alla", "Mirem", ["Ahemd", "Amer"]],
-];
-
-let [, , , , [, , [a, b]]] = myFriends;
-console.log(a);
-console.log(b);
-
 //console.log(myFriends[4][2][1])
-
+console.clear();
 const myobject = {
   a: 1,
   b: 2,
 };
 //       تعريف الملكية
 Object.defineProperty(myobject, "c", {
-  writable: false,  // schreibbar // auf false ===> قابل للكتابة // ===> في حالة التغيير ، لا يمكن للمستخدم تغييره 
+  writable: false, // schreibbar // auf false ===> قابل للكتابة // ===> في حالة التغيير ، لا يمكن للمستخدم تغييره
   enumerable: true, //معدود   aufzählbar // wenn das auf false ändern wollen dann wrid es es vom loop nicht anerkennt  (c) nur
   configurable: true, // شكلي konfigurierbar
   value: 3,
 });
-    myobject.c = 100;
+myobject.c = 100;
 
-    for(let mains in myobject){
-        console.log(mains, myobject[mains])
-    }
+for (let mains in myobject) {
+  console.log(mains, myobject[mains]);
+}
 
-console.log(myobject)
+console.log(myobject);
