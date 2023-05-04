2015d6478cf169b4b570d87471e71d0aca63eacc
alisawiali <alisawiali70@gmail.com>
2023-05-04 12:04:13 +0200

    Rebort

---
+++ Rebort/closure.js
@@ -0,0 +1,39 @@
+console.clear();
+
+const shop = () => {
+  const dh = ["Imad", "Ali", "Alisawi"];
+  const printItem = () => {
+    dh.forEach((itme) => console.log(itme));
+  };
+  const addItem = (x) => {
+    dh.push(x.toUpperCase());
+  };
+  return { printItem, addItem };
+};
+let funck = shop();
+// console.log(funck)
+funck.printItem("hallo");
+funck.addItem("hallo");
+//-----------------------------------------------------------------------
+function sumTo(n) {
+  let sum = 0;
+  for (let i = 0; i < n; i++) {
+    sum += i;
+  }
+  return sum;
+}
+console.log(sumTo(4));
+//For Loop startet bei 0 und stoppt bei 4: 0+1+2+3 => 6
+function names(n) {
+  if (n === 0) {
+    return 1;
+  }
+  return n + names(n - 1);
+}
+
+let result = names(5);
+console.log(result);
+//For Loop startet bei 0 und stoppt bei 5: 0+1+2+3+4 = 10
+
+
+
