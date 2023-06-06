fffe107f24a8b1953e3c28965898e526a78dd086
alisawiali <alisawiali70@gmail.com>
2023-06-06 16:04:59 +0200

    Do test

---
+++ Dominick/dennistag./main.js
@@ -0,0 +1,79 @@
+console.clear();
+const a = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+const b = [
+  ".-",
+  "-...",
+  "-.-.",
+  "-..",
+  ".",
+  "..-.",
+  "--.",
+  "....",
+  "..",
+  ".---",
+  "-.-",
+  ".-..",
+  "--",
+  "-.",
+  "---",
+  ".--.",
+  "--.-",
+  ".-.",
+  "...",
+  "-",
+  "..-",
+  "...-",
+  ".--",
+  "-..-",
+  "-.--",
+  "--..",
+];
+
+function tarnslate(text) {
+  let result = "";
+
+  for (let f = 0; f < text.length; f++) {
+    const letter = text[f];
+
+    const head = a.indexOf(letter);
+
+    if (head < 0) {
+      result += "    ";
+      continue;
+    }
+
+    const morsLetter = b[head];
+    result += " " + morsLetter;
+  }
+
+  return result;
+}
+
+console.log(tarnslate("nanu, was passiert denn hier?"));
