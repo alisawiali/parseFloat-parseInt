d1fc35673a3bb5e43845de3c1922ef3e760a2e67
alisawiali <alisawiali70@gmail.com>
2023-06-06 13:20:13 +0200

    DOM Event

--- DOM/Next-Previous-sibling/sibling.js
+++ DOM/Next-Previous-sibling/sibling.js
@@ -7,19 +7,19 @@
 
 const myMain = document.getElementById("main");
 
-console.log(myMain.childNodes);
-console.log(myMain.firstElementChild); // p
-console.log(myMain.firstElementChild.nextSibling); //Secound Text
-console.log(myMain.firstElementChild.previousSibling); // First text
-console.log(myMain.lastElementChild.previousSibling); // Secound Text
-console.log(myMain.lastElementChild.nextSibling); // Fianl Text
+// console.log(myMain.childNodes);
+// console.log(myMain.firstElementChild); // p
+// console.log(myMain.firstElementChild.nextSibling); //Secound Text
+// console.log(myMain.firstElementChild.previousSibling); // First text
+// console.log(myMain.lastElementChild.previousSibling); // Secound Text
+// console.log(myMain.lastElementChild.nextSibling); // Fianl Text
 
 const mybtn = document.getElementById("test");
 console.log(mybtn);
 
 mybtn.onclick = function () {
   if (myMain.firstElementChild.nextElementSibling !== "Secound Text") {
-    myMain.firstElementChild.nextElementSibling.style.color = "red";
+    myMain.firstElementChild.style.color = "red";
   } else {
     console.log("versuch es nochmal");
   }
